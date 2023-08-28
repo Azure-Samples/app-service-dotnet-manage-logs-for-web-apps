3cce6ea57997c2ed2626d1e0e2b8946f2fb1b58f
mcgallan <1186003288@qq.com>
2023-08-28 10:45:51 +0800

    update

--- Program.cs
+++ Program.cs
@@ -1,14 +1,20 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using Microsoft.Azure.Management.AppService.Fluent;
-using Microsoft.Azure.Management.Fluent;
-using Microsoft.Azure.Management.ResourceManager.Fluent;
-using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
-using Microsoft.Azure.Management.Samples.Common;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.AppService;
+using Azure.ResourceManager.AppService.Models;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Samples.Common;
+using Azure.ResourceManager.Sql;
+using Azure.ResourceManager.Sql.Models;
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ManageWebAppLogs
@@ -22,14 +28,16 @@ namespace ManageWebAppLogs
          *    - stream logs for 120 seconds
          */
 
-        public static void RunSample(IAzure azure)
+        public static async Task RunSample(ArmClient client)
         {
             // New resources
+            AzureLocation region = AzureLocation.EastUS;
             string suffix         = ".azurewebsites.net";
-            string appName       = SdkContext.RandomResourceName("webapp1-", 20);
+            string appName       = Utilities.CreateRandomName("webapp1-");
             string appUrl        = appName + suffix;
-            string rgName         = SdkContext.RandomResourceName("rg1NEMV_", 24);
-
+            string rgName         = Utilities.CreateRandomName("rg1NEMV_");
+            var lro = await client.GetDefaultSubscription().GetResourceGroups().CreateOrUpdateAsync(Azure.WaitUntil.Completed, rgName, new ResourceGroupData(AzureLocation.EastUS));
+            var resourceGroup = lro.Value;
             try {
 
 
@@ -38,61 +46,67 @@ namespace ManageWebAppLogs
 
                 Utilities.Log("Creating web app " + appName + " in resource group " + rgName + "...");
 
-                IWebApp app = azure.AppServices.WebApps.Define(appName)
-                        .WithRegion(Region.USWest)
-                        .WithNewResourceGroup(rgName)
-                        .WithNewWindowsPlan(PricingTier.BasicB1)
-                        .WithJavaVersion(JavaVersion.V8Newest)
-                        .WithWebContainer(WebContainer.Tomcat8_0Newest)
-                        .DefineDiagnosticLogsConfiguration()
-                            .WithWebServerLogging()
-                            .WithWebServerLogsStoredOnFileSystem()
-                            .Attach()
-                        .Create();
-
-                Utilities.Log("Created web app " + app.Name);
-                Utilities.Print(app);
+                var webSiteCollection = resourceGroup.GetWebSites();
+                var webSiteData = new WebSiteData(region)
+                {
+                    SiteConfig = new Azure.ResourceManager.AppService.Models.SiteConfigProperties()
+                    {
+                        WindowsFxVersion = "PricingTier.StandardS1",
+                        NetFrameworkVersion = "NetFrameworkVersion.V4_6",
+                    }
+                };
+                var webSite_lro = await webSiteCollection.CreateOrUpdateAsync(Azure.WaitUntil.Completed, appName, webSiteData);
+                var webSite = webSite_lro.Value;
+                Utilities.Log("Created web app " + webSite.Data.Name);
+                Utilities.Print(webSite);
 
                 //============================================================
                 // Listen to logs synchronously for 30 seconds
 
-                using (var stream = app.StreamAllLogs())
+                var csm = new CsmPublishingProfile()
+                {
+                    Format = PublishingProfileFormat.Ftp
+                };
+                var stream_lro = await webSite.GetPublishingProfileXmlWithSecretsAsync(csm);
+                using (var stream = stream_lro.Value)
                 {
                     var reader = new StreamReader(stream);
                     Utilities.Log("Streaming logs from function app " + appName + "...");
-                    string line = reader.ReadLine();
+                    string? line = reader.ReadLine();
                     Stopwatch stopWatch = new Stopwatch();
                     stopWatch.Start();
-                    Task.Factory.StartNew(() =>
+                    await Task.Factory.StartNew(async () =>
                     {
                         //============================================================
                         // Deploy to app 1 through zip deploy
 
-                        SdkContext.DelayProvider.Delay(10000);
+                        Thread.Sleep(10000);
                         Utilities.Log("Deploying coffeeshop.war to " + appName + " through web deploy...");
 
-                        app.Deploy()
-                            .WithPackageUri("https://github.com/Azure/azure-libraries-for-java/raw/master/azure-samples/src/main/resources/coffeeshop.zip")
-                            .WithExistingDeploymentsDeleted(false)
-                            .Execute();
+                        var extensionResource = webSite.GetSiteExtension();
+                        var deployresource = await extensionResource.CreateOrUpdateAsync(WaitUntil.Completed, new WebAppMSDeploy()
+                        {
+                            PackageUri = new Uri("https://github.com/Azure/azure-libraries-for-java/raw/master/azure-samples/src/main/resources/coffeeshop.zip"),
+                            IsAppOffline = false,
+                        }) ;
 
-                        Utilities.Log("Deployments to web app " + app.Name + " completed");
-                        Utilities.Print(app);
+                        Utilities.Log("Deployments to web app " + webSite.Data.Name + " completed");
+                        Utilities.Print(webSite);
 
                         // warm up
                         Utilities.Log("Warming up " + appUrl + "/coffeeshop...");
                         Utilities.CheckAddress("http://" + appUrl + "/coffeeshop");
 
-                        SdkContext.DelayProvider.Delay(5000);
+                        Thread.Sleep(5000);
                         Utilities.Log("CURLing " + appUrl + "/coffeeshop...");
                         Utilities.Log(Utilities.CheckAddress("http://" + appUrl + "/coffeeshop"));
-                        SdkContext.DelayProvider.Delay(15000);
+                        Thread.Sleep(15000);
                         Utilities.Log("CURLing " + appUrl + "/coffeeshop...");
                         Utilities.Log(Utilities.CheckAddress("http://" + appUrl + "/coffeeshop"));
-                        SdkContext.DelayProvider.Delay(25000);
+                        Thread.Sleep(25000);
                         Utilities.Log("CURLing " + appUrl + "/coffeeshop...");
                         Utilities.Log(Utilities.CheckAddress("http://" + appUrl + "/coffeeshop"));
-                        SdkContext.DelayProvider.Delay(35000);
+                        Thread.Sleep(35000);
                         Utilities.Log("CURLing " + appUrl + "/coffeeshop...");
                         Utilities.Log(Utilities.CheckAddress("http://" + appUrl + "/coffeeshop"));
                     });
@@ -108,7 +122,7 @@ namespace ManageWebAppLogs
                 try
                 {
                     Utilities.Log("Deleting Resource Group: " + rgName);
-                    azure.ResourceGroups.DeleteByName(rgName);
+                    await resourceGroup.DeleteAsync(WaitUntil.Completed);
                     Utilities.Log("Deleted Resource Group: " + rgName);
                 }
                 catch (NullReferenceException)
@@ -122,24 +136,23 @@ namespace ManageWebAppLogs
             }
         }
 
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             try
             {
                 //=================================================================
                 // Authenticate
-                var credentials = SdkContext.AzureCredentialsFactory.FromFile(Environment.GetEnvironmentVariable("AZURE_AUTH_LOCATION"));
-
-                var azure = Azure
-                    .Configure()
-                    .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
-                    .Authenticate(credentials)
-                    .WithDefaultSubscription();
+                var clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
+                var clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
+                var tenantId = Environment.GetEnvironmentVariable("TENANT_ID");
+                var subscription = Environment.GetEnvironmentVariable("SUBSCRIPTION_ID");
+                ClientSecretCredential credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+                ArmClient client = new ArmClient(credential, subscription);
 
                 // Print selected subscription
-                Utilities.Log("Selected subscription: " + azure.SubscriptionId);
+                Utilities.Log("Selected subscription: " + client.GetSubscriptions().Id);
 
-                RunSample(azure);
+                await RunSample(client);
             }
             catch (Exception e)
             {
